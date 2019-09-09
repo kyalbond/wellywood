0d582c5d4a5d1633c9b96e9f7d7b450f563f607f
bondkyal <bondkyal@vuw.ac.nz>
2019-09-09 15:24:02 +1200

    Setup basic layout of app

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wellywood/account_settings/settings.dart';
+import 'package:wellywood/help/help.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,8 +11,48 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'))
-    );
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('WellyWood'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Dasboard'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Settings'),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Settings()));
+                },
+              ),
+              ListTile(
+                title: Text('Help'),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Help()));
+                },
+              ),
+              ListTile(
+                title: Text('Logout'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
