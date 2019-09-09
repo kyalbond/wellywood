8da8b7932bef6f147640c29f873ea632e6c6d237
bondkyal <bondkyal@vuw.ac.nz>
2019-09-09 13:46:56 +1200

    Initial commit with login screen

---
+++ lib/login/login.dart
@@ -0,0 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:wellywood/home/home.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sign in'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please provide an email';
+                  }
+                  return '';
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 7) {
+                    return 'Your password needs to be atleast 8 characters';
+                  }
+                  return '';
+                },
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: signIn,
+                child: Text('Sign in'),
+              )
+            ],
+          ),
+        ));
+  }
+
+  Future<void> signIn() async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        Navigator.push(
+            (context), MaterialPageRoute(builder: (context) => Home()));
+      } catch (e) {
+        print(e.message);
+      }
+    } else {
+      print('fail');
+    }
+  }
+}
