8fa86ce1bba77125d793d1167b1374ba99893abc
bondkyal <bondkyal@vuw.ac.nz>
2019-09-10 21:56:08 +1200

    commit before dylan breaks

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,29 +3,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:wellywood/models/user.dart';
 
-class Database {
-  
-  Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
-  }
+Future<void> signOut() async {
+  await FirebaseAuth.instance.signOut();
+}
 
-  User getUser(uid) {
-    try {
-      Firestore.instance
-          .collection("users")
-          .document(uid)
-          .get()
-          .then((DocumentSnapshot ds) {
-        return new User(ds.data['username'], ds.data['userIcon']);
-      });
-    } catch (e) {
-      print('Failed to get user: ' + e);
-    }
-    return null;
+User getUser(uid) {
+  try {
+    Firestore.instance
+        .collection("users")
+        .document(uid)
+        .get()
+        .then((DocumentSnapshot ds) {
+      return new User(ds.data['username'], ds.data['userIcon']);
+    });
+  } catch (e) {
+    print('Failed to get user: ' + e);
   }
+  return null;
 }
 
 class GetPosts extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -37,24 +35,35 @@ class GetPosts extends StatelessWidget {
             return new Text('Loading...');
           default:
             return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                return new ListTile(
-                    title: Text(document['user']),
-                    subtitle: Text(
-                        (document['date'] as Timestamp).toDate().toString()),
-                    trailing: (true == false)
-                        ? (CircleAvatar(
-                            //TODO: UTILISE USER ICON
-                            ))
-                        : (CircleAvatar(
-                            backgroundColor: Colors.black,
-                            child: Text(document['user'].toString()[0]),
-                          )));
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+
+                return Column(
+                  children: <Widget>[
+                    Image.network(document['image']),
+                    ListTile(
+                      title: Text(document['user']),
+                      subtitle: Text((document['date'] as Timestamp).toDate().toString()),
+                      trailing: (true == false)
+                          ? (CircleAvatar(
+                              //TODO: UTILISE USER ICON
+                              ))
+                          : (CircleAvatar(
+                              backgroundColor: Colors.black,
+                              child: Text(document['user'].toString()[0]),
+                            )),
+                      onLongPress: goToMap(),
+                    ),
+                  ]
+                );
               }).toList(),
             );
         }
       },
     );
   }
+
+}
+
+goToMap() {
+
 }
