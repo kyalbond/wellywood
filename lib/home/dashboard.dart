e07c8525b30c7ce4b9dd12afec635fb7c8d8a279
bondkyal <bondkyal@vuw.ac.nz>
2019-09-10 18:18:53 +1200

    Parsed timestamp => date and added user icon

--- lib/home/dashboard.dart
+++ lib/home/dashboard.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:wellywood/services/firestore.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -8,27 +7,36 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('posts').snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting: return new Text('Loading...');
+          case ConnectionState.waiting:
+            return new Text('Loading...');
           default:
             return new ListView(
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
                 return new ListTile(
-                  title: new Text(document['user']),
-                  subtitle: new Text(document['likes'].toString()),
-                );
+                    title: Text(document['user']),
+                    subtitle: Text((document['date'] as Timestamp).toDate().toString()),
+                    trailing: (true == false) 
+                        ? (CircleAvatar(
+                            //TODO: UTILISE USER ICON
+                          ))
+                        : (CircleAvatar(
+                            backgroundColor: Colors.black,
+                            child: Text(document['user'].toString()[0]),
+                          ))
+                    );
               }).toList(),
             );
         }
       },
     );
   }
-
 }
